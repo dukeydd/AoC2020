0bc8e0834fb03671a3628633961e2fb3713cdf4b
David Duke <d@dcd.im>
2020-12-13 13:23:26 +0000

    completed day5 part 1&2

--- day5/day5.go
+++ day5/day5.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"strings"
+	"sort"
 )
 
 func getInput() ([]string, int) {  
@@ -22,27 +23,81 @@ func getIDs(input string) (string, string) {
 	return rowID, columnID
 }
 
-func getNewRange(upper int, lower int, s string) (int, int) {
+func getNewRange(front int, back int, s string) (int, int) {
 	// this needs to be changed to add or subtract half difference of upper-lower not /2
-	if s == "F" {
-		return upper, int(lower/2)
-	} else if s == "B" {
-		return int(upper/2), lower
+	diff := back - front + 1
+	
+	if s == "F" || s == "L" {
+		return front, (back - diff/2)
+	} else if s == "B" || s == "R" {
+		return (front + diff/2), back
 	} else {
 		fmt.Println("input incorrect")
 		panic("ahh")
-	}
+	}	
 }
 
-// func calculateSeatLoc(id string, columnId string) int {
-// 	for i:=0; i<len(id)
-// }
+func getNums(rowID string, columnID string) (int, int) {
+	var rowNum int
+	var columnNum int
+	// find row number
+	currentFront, currentBack := 0, 127
+	for j:=0; j<len(rowID); j++ {
+		currentFront, currentBack = getNewRange(currentFront, currentBack, string(rowID[j]))
+	}
+	// check they are the same
+	if currentBack == currentFront {
+		rowNum = currentFront
+	} else {
+		fmt.Println("something went wrong")
+	}
+
+	// find column number
+	currentLeft, currentRight := 0, 7
+	for k:=0; k<len(columnID); k++ {
+		currentLeft, currentRight = getNewRange(currentLeft, currentRight, string(columnID[k]))
+	}
+	// check they are the same
+	if currentLeft == currentRight {
+		columnNum = currentLeft
+	} else {
+		fmt.Println("something went wrong")
+	}
+	return rowNum, columnNum
+}
+
+func calculateSeatID(rowNum int, columnNum int) int {
+	return rowNum*8 + columnNum
+}
+
+func getMaxInt(nums []int) int {
+	var m int
+	for i, e := range nums {
+		if i==0 || e > m {
+			m = e
+		}
+	}
+	return m 
+}
 
 func main() {
 	dataList, dataRange := getInput()
+
+	var seatIDs []int 
 	for i:=0; i<dataRange; i++ {
 		rowID, columnID := getIDs(dataList[i])
-		newUp, newLow := getNewRange(0, 127, "B")
-		fmt.Println(newUp, newLow)
+		rowNum, columnNum := getNums(rowID, columnID)
+		seatIDs = append(seatIDs, calculateSeatID(rowNum, columnNum))
+	}
+
+	sort.Ints(seatIDs)
+	// fmt.Println(seatIDs)
+	for j:=0; j<len(seatIDs); j++ {
+		if j != 0 {
+			if seatIDs[j+1] != seatIDs[j] +1 {
+				fmt.Println(seatIDs[j] + 1)
+				return
+			}
+		}
 	}
 }
