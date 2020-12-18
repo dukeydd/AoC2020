e2444dbdde50c7c0d5be6c67843cf2f491992c60
David Duke <d@dcd.im>
2020-12-18 12:56:18 -0800

    part2

--- nodejs/day2/day2.js
+++ nodejs/day2/day2.js
@@ -1,10 +1,10 @@
 const readFileInput = require('../helpers/readFileInput');
 const data = readFileInput.getPasswords();
 
+// Part 1
 function getValidPasswords(data) {
     let valid = 0;
     data.forEach(entry => {
-        // if (entry.pswd.charAt(entry.quant[0]-1) === entry.letter && entry.pswd.charAt(entry.quant[1]-1) === entry.letter)
         let letterCount = 0;
         for (let i = 0; i < entry.pswd.length; i++) {
             if (entry.pswd.charAt(i) === entry.letter) {
@@ -18,4 +18,21 @@ function getValidPasswords(data) {
     console.log(valid)
 }
 
-getValidPasswords(data);
+// Part 2
+function getValidPasswordsPart2(data) {
+    let valid = 0;
+    data.forEach(entry => {
+        let correctIndexes = 0;
+        if (entry.pswd.charAt(entry.quant[0]-1) === entry.letter) {
+            correctIndexes++;
+        }
+        if (entry.pswd.charAt(entry.quant[1]-1) === entry.letter) {
+            correctIndexes++;
+        }
+        if (correctIndexes == 1) {
+            valid++;
+        }
+    });
+    console.log(valid)
+}
+getValidPasswordsPart2(data);
